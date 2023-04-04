d3f2266885195fb75529fa28ca8e372635f1f31b
unknown <leoveiga2002@gmail.com>
2023-04-04 08:26:33 -0300

    Updates

--- js/381.aa638b88.js
+++ js/381.aa638b88.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkteste"]=self["webpackChunkteste"]||[]).push([[381],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var s=n(3396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function c(t,e){return(0,s.wg)(),(0,s.iD)("div",u,r)}var i=n(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=381.34f5e3dd.js.map
+//# sourceMappingURL=381.aa638b88.js.map
